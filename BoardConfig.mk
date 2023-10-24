ashutoshchettri/android_device_xiaomi_alioth

alioth: Update kernel defconfig

BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
+TARGET_KERNEL_CONFIG += vendor/alioth_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := alioth,aliothin

halcyonlv/device_xiaomi_alioth

BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/alioth.config
+TARGET_KERNEL_CONFIG += vendor/zenith_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := alioth,aliothin