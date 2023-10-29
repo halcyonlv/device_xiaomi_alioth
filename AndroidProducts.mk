bf41069052ebd8118f056de336fdd57f93d97d39
ashutoshchettri <eidoronp67@gmail.com>
2023-10-29 21:07:31 +0545

    alioth: Inherit for `Halcyon`

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/halcyon_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    halcyon_alioth-user \
+    halcyon_alioth-userdebug \
+    halcyon_alioth-eng
--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/products/miuicamera.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-halcyon
 
 PRODUCT_PACKAGES += \
     AliothFrameworks \
