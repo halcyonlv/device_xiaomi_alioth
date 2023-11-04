bab0479396048803d7945a1709c6635ed79d3557
ashutoshchettri <eidoronp67@gmail.com>
2023-11-04 10:03:25 +0545

    [SQUASH] alioth: Drop dolby
    Revert "alioth: Drop Xiaomi Audio Policy Engine Configurations"
    
    This reverts commit 864ce1df8864d9e02ad5da649d4f34a2ca73c928.
    
    Revert "alioth: Audio: Import Audio Policy Engine Configurations from Miui 13.0.9.0 EU"
    
    This reverts commit beae735576f0d537e1c2ebbe724ed06437491b4e.
    
    Revert "alioth: Import Dolby Config"
    
    This reverts commit 43fbb7aa330d08f140c8651f14413fb5ec3f8c3f.
    
    Revert "alioth: Import DaxUI (Dolby UI) from OnePlus"
    
    This reverts commit 4789db928843addfa522a34a568246740260aa26.
    
    alioth: Drop dolby

--- device.mk
+++ device.mk
@@ -32,13 +32,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-# Dolby Support
-TARGET_USES_MIUI_DOLBY := true
-
-# Dolby Config File
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
-
 # Miuicamera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/products/miuicamera.mk)
 
